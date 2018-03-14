393c75509c29b462c2c6feb2d6b45a8edc6d02dd
hasankole <36761604+hasankole@users.noreply.github.com>
2018-03-14 16:50:50 +0300

    Create hw4.py

---
+++ week2/hw4.py
@@ -0,0 +1,16 @@
+a = []
+s = input("Polidromu denediğiniz kelimeyi veya Cümleyi giriniz : ")
+
+
+my_str = s
+
+my_str = my_str.casefold()
+
+rev_str = reversed(my_str)
+
+if list(my_str) == list(rev_str):
+    print("polidromdur")
+
+else:
+    print("polidrom değildir")
+© 2018 GitHub, Inc.
